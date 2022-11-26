d995a2e017fb50e35cd2194439d6aae664863543
exionite <exionite@gmail.com>
2022-11-25 16:20:24 -0800

    login

--- src/components/loginAuth/LoginAuth.js
+++ src/components/loginAuth/LoginAuth.js
@@ -3,21 +3,22 @@ import { useSelector, useDispatch } from 'react-redux';
 import { authActions } from '../../store/auth';
 
 export default function LoginAuth() {
+  const dispatch = useDispatch();
   const isAuth = useSelector((state) => state.auth.isAuthenticated);
   const login = authActions.login;
   const logout = authActions.logout;
 
   const loginHandler = () => {
-    login();
+    dispatch(login())
   }
   const logoutHandler = () => {
-    logout();
+    dispatch(logout());
   }
 
   return (
     <div>
       LoginAuth
-      <button onClick={() => loginHandler}>Login</button>
+      <button onClick={loginHandler}>Login</button>
       <button onClick={logoutHandler}>Logout</button>
       {isAuth && <div>AUTHORIZATION GRANTED. CONGRATULATIONS</div>}
     </div>
